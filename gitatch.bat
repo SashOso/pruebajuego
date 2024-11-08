7078eb290a84a3b75f984209a077bd84dc2ae591
andrestch <andrestuesta_22@outlook.com>
2024-11-08 18:37:58 -0500

    primer comit

---
+++ gitatch.bat
@@ -0,0 +1,69 @@
+@echo off
+cls
+
+:MENU
+cls
+echo ================================
+echo 1. Guardar Cambios
+echo 2. Subir a GitHub
+echo 3. Configurar
+echo 4. Salir
+echo ================================
+set /p opcion="Opcion: "
+if "%opcion%"=="1" goto GUARDAR
+if "%opcion%"=="2" goto SUBIR
+if "%opcion%"=="3" goto CONFIG
+if "%opcion%"=="4" goto SALIR
+goto MENU
+
+
+:SUBIR
+echo --------------------------------
+git init
+git add .
+echo --------------------------------
+set /p commit="Commit: "
+echo --------------------------------
+git commit -m "%commit%"
+echo --------------------------------
+set /p repositorio="Repositorio: "
+echo --------------------------------
+git remote add origin %repositorio%
+echo --------------------------------
+set /p rama="Rama: "
+echo --------------------------------
+git push -u origin %rama%
+echo --------------------------------
+pause
+goto MENU
+
+:GUARDAR
+echo --------------------------------
+git add .
+set /p commit="Commit: "
+echo --------------------------------
+git commit -m "%commit%"
+echo -------------------------------
+git push
+echo --------------------------------
+pause
+goto MENU
+
+
+:CONFIG
+echo --------------------------------
+set /p name="Name: "
+git config --global user.name "%name%"
+echo --------------------------------
+set /p email="Email: "
+git config --global user.email "%email%"
+echo --------------------------------
+pause
+goto MENU
+
+
+:SALIR
+cls
+exit
+
+
